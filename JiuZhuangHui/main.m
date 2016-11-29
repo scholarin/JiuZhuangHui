1b82b7effdde3ceeeb487d51996cea6eb5330294
scholarin <Mosin@192.168.0.103>
2016-11-29 22:28:51 +0800

    Initial Commit

---
+++ JiuZhuangHui/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JiuZhuangHui
+//
+//  Created by MosinNagant on 2016/11/29.
+//  Copyright © 2016年 Mosin Nagant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
